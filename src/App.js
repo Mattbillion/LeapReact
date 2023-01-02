c02bd1ad32f8787d8acffe4ed8da0c52da3188d0
Morning <morning@Huslees-iMac-2.local>
2023-01-02 12:27:29 +0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import './App.css';
 
 function App() {
 
-  const name = 'Legendary Muugii';
+  let name = 'Legendary Muugii';
+  const name1 = 'Buzz Muugii'
+  
   
   const customStyle = {
     color: name.includes("Legendary") ? "#f3c701" : "#ffffff",
     fontSize: name.includes("Legendary") ? "56px" : "24px"
   }
 
+
   function welcomeMessage() {
-    if (name.includes("Legendary")) {
-      return <h1 style={customStyle}>{name}!</h1>
+    if(name.includes("Legendary")) {
+      return <h1 style={customStyle}>{name}</h1>
     }
-    return <h1 style={customStyle}>{name}!</h1>
+    if(name.includes("Buzz")) {
+      return <h1 style={customStyle}>{name}</h1>
+    }
+  }
+
+  const changeName = () => {
+    name += "Chad"
+    return <h1>{welcomeMessage()}</h1>
   }
   return (
     <div className="App">
      
       <header className="App-header">
         <h1>{welcomeMessage()} </h1>
+        <button onClick={changeName()}>Change name</button>
         <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
